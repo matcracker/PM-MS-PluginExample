bbf69ae4559fd1a47b5a5ed8d751dfe7a7013673
matcracker <matcracker98@gmail.com>
2016-05-17 15:23:50 +0200

    Added ImagicalMineInstaller!

---
+++ ImagicalMineInstaller/src/com/matcraker/ImagicalMineInstaller/Main/Main.java
@@ -0,0 +1,158 @@
+package com.matcraker.ImagicalMineInstaller.Main;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.matcracker.PMManagerServers.API.PluginInformation;
+import com.matcracker.PMManagerServers.API.PluginStarter;
+import com.matcracker.PMManagerServers.API.StatusAPI;
+import com.matcracker.PMManagerServers.API.UtilityServersAPI;
+import com.matcracker.PMManagerServers.Installer.ManagerInstaller;
+import com.matcracker.PMManagerServers.Languages.BaseLang;
+import com.matcracker.PMManagerServers.Settings.PluginManager;
+import com.matcracker.PMManagerServers.Utility.FileChooser;
+import com.matcracker.PMManagerServers.Utility.Utility;
+import com.matcracker.PMManagerServers.Utility.UtilityColor;
+
+
+public class Main implements PluginStarter, PluginInformation{
+	
+	public String title = "&3============================&6<&bImagicalMine Installer&6>&3============================&f";
+	private String link = "http://imagicalcorp.github.io/ImagicalMine/service-download/ImagicalMine.phar";
+
+	@Override
+	public void execute() {
+		menu();
+	}
+	
+	private void menu(){
+		Utility.cleanScreen();
+		System.out.println(title);
+		System.out.println("1- Download ImagicalMine");
+		System.out.println("2- Install ImagicalMine");
+		System.out.println("3- Back");
+		int sel = Utility.readInt("Select option: ", null);
+		
+		try{
+			if(sel == 1)
+				download();
+			
+			if(sel == 2)
+				install();
+			
+			if(sel == 3)
+				PluginManager.plugMenu();
+			
+		}catch(IOException e){}
+	}
+
+	private void download() throws IOException {
+		Utility.cleanScreen();
+		System.out.println(title);
+		System.out.println("1- Download latest version {MCPE: 0.14.2}");
+		System.out.println("2- " + BaseLang.translate("pm.standard.back"));
+		int sel = Utility.readInt("Select version: ", null);
+		
+		if(sel == 1){
+			int server = 0;
+			
+			Utility.showServers();
+			server = Utility.readInt("Select server: ", null);
+			
+			if(server <= UtilityServersAPI.getNumberServers()){
+				File imagical = new File("Utils" + File.separator + "PocketMine-MP_IMAGICALMINE.phar");
+				if(!imagical.exists()){
+					if(UtilityServersAPI.checkServersFile("Path", "downloadPath", -1)){
+						System.out.println("Downloading latest phar...");
+						Utility.openSoftware("url", link);
+						StatusAPI.setStatus(BaseLang.translate("pm.status.download"), server);
+						Utility.waitConfirm("ImagicalMine downloaded! Now install it!");
+						ManagerInstaller.moveDownloadedFiles(UtilityServersAPI.getDownloadPath(), "ImagicalMine.phar", "IMAGICALMINE");
+					}else
+						UtilityServersAPI.setDownloadPath(FileChooser.get("Select download path", "All", ""));
+				}else
+					Utility.waitConfirm("You've already downloaded this version");
+				
+			}else
+				Utility.waitConfirm("Select an avaiable server");
+			
+			download();
+		}
+		menu();
+	}
+
+	private void install() {
+		Utility.cleanScreen();
+		System.out.println(title);
+		System.out.println("1- Install latest version {MCPE: 0.14.2}");
+		System.out.println("2- " + BaseLang.translate("pm.standard.back"));
+		int sel = Utility.readInt("Select version: ", null);
+		
+		if(sel == 1){
+			int server = 0;
+			
+			Utility.showServers();
+			server = Utility.readInt("Select server: ", null);
+			
+			if(server <= UtilityServersAPI.getNumberServers()){
+				File imagical = new File("Utils" + File.separator + "PocketMine-MP_IMAGICALMINE.phar");
+				if(imagical.exists()){
+					if(UtilityServersAPI.checkServersFile("Path", "path_", server)){
+						String pathContent = UtilityServersAPI.getPath(server);
+						if(pathContent != null){
+							System.out.println("Installing ImagicalMine...");
+							try{
+								ManagerInstaller.changeInstallationsFile(pathContent, "IMAGICALMINE");
+							}catch (IOException e){
+								e.printStackTrace();
+							}
+							StatusAPI.setVersion("ImagicalMine", server);
+							Utility.waitConfirm("ImagicalMine installed!");
+						}else
+							Utility.waitConfirm(BaseLang.translate("pm.installer.pharNotFound"));
+					}else
+						Utility.waitConfirm(BaseLang.translate("pm.errors.pathNotFound"));	
+				}else
+					Utility.waitConfirm("You don't download ImagicalMine! First download it!");
+				
+			}else
+				Utility.waitConfirm("Select an avaiable server");
+			
+			 install();
+		}
+		menu();
+	}
+
+	@Override
+	public void onDisable() {
+		System.out.println(UtilityColor.COLOR_GREEN + this.getName() + " " + "v" + this.getVersion() + " is disabled successfully!");
+		
+	}
+
+	@Override
+	public void onEnable() {
+		System.out.println(UtilityColor.COLOR_GREEN + this.getName() + " " + "v" + this.getVersion() + " is enabled successfully!");
+		
+	}
+
+	@Override
+	public String getAPIVersion() {
+		return "1.0";
+	}
+
+	@Override
+	public String getAuthor() {
+		return "matcracker";
+	}
+
+	@Override
+	public String getName() {
+		return "ImagicalMine-Installer";
+	}
+
+	@Override
+	public String getVersion() {
+		return "1.0";
+	}
+
+}
